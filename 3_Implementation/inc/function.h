52a309cf924b1e24969e7edb899a14cd4bad4356
SaiRatnakarGit <19a85a0312@sves.org.in>
2022-03-30 14:02:10 +0530

    added implementation

---
+++ 3_Implementation/inc/function.h
@@ -0,0 +1,6 @@
+void red();
+void yellow();
+void reset();
+void colour_char(char c);
+
+void print_pattern(char board[]);
---
+++ 3_Implementation/inc/test.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "brain.h"
+
+int main(){
+
+ char board[] = {'1', '2', '3', '4', '5', '6', 'X', 'X', 'X'};
+
+
+if(check(board)==1){
+
+return  0;
+}else{
+
+return 1;
+}
+
+
+
+
+
+}
